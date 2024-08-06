0cdca029ae68251278b76ecf1d7b285b24ac7996
Sinspired <63581268+sinspired@users.noreply.github.com>
2024-08-06 20:46:58 +0800

    Create fetch_vpngate.py

---
+++ fetch_vpngate.py
@@ -0,0 +1,11 @@
+import requests
+import csv
+
+url = "https://www.vpngate.net/api/iphone"
+response = requests.get(url)
+data = response.text.splitlines()
+
+with open('list.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    for line in data:
+        csvwriter.writerow(line.split(','))
